8765260b9ea97f8a67fb12e5d4efeaeb315d6e12
Xtreme <X@gmail.com>
2025-07-13 13:37:16 +0330

    main : More readable output

--- Mahtab/main.py
+++ Mahtab/main.py
@@ -5,16 +5,16 @@ images_path = "/home/mohsen/Desktop/homelab/mahta/test-data"
 image_extensions = ('.jpg', '.jpeg', '.png', '.bmp', '.tiff', '.tif', '.webp')
 image_list = [f for f in os.listdir(images_path) if f.lower().endswith(image_extensions)]
 
+
 visualize_input = input("Visualize? [y|n]: ").strip().lower()
 visualize = visualize_input in ['y', 'yes']
 
-print("#" * 69)
 
 easyocr_model = easyocr_core.easyOCR(visualize)
 tesseract_model = tesseract_core.tesseractOCR(visualize)
 
 for i, image in enumerate(image_list, 1):
-    print(f"[{i}/{len(image_list)}] {image}")
+    print("#" * 30, f"[{i}/{len(image_list)}] {image}", "#" * 30)
     
     image_path = os.path.join(images_path, image)
     
@@ -22,8 +22,18 @@ for i, image in enumerate(image_list, 1):
         result_easyocr = easyocr_model.detect_text(image_path)
         result_tesseract = tesseract_model.detect_text(image_path)
         
-        print(f"EasyOCR: {result_easyocr}")
-        print(f"Tesseract: {result_tesseract}")
+        print(f"EasyOCR recoginzed {len(result_easyocr)} bounding boxes.")
+        print(f"Tesseract recoginzed {len(result_tesseract)} bounding boxes.")
+
+        print("EasyOCR:\n")
+        for result in result_easyocr:
+            print(result)
+
+        print("#" * 30)
+
+        print("Tesseract:\n")
+        for result in result_tesseract:
+            print(result)
         
         if result_easyocr == result_tesseract:
             print("Match")
